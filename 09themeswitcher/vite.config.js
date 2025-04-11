b91da49d5ccc3267d6872d5ccb18d0392454307a
PRNC01 <prnc.business23@gmail.com>
2025-04-11 13:18:43 +0000

    Context API Theme Switcher

--- 09themeswitcher/vite.config.js
+++ 09themeswitcher/vite.config.js
@@ -2,15 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://vite.dev/config/
 export default defineConfig({
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  darkMode: "class",
-  theme: {
-    extend: {},
-  },
-  plugins: [react(), tailwindcss(),],
+  plugins: [react(), tailwindcss({
+    // Add Tailwind configuration here
+    config: {
+      content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+      ],
+      darkMode: "class", // This enables class-based dark mode
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    }
+  }),],
 })
